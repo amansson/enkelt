78277f6e2d0970de36753d95e37d5eb7e379f5e4
Rano Kuhl <ranokuhl@gmail.com>
2019-06-17 11:44:03 +0200

    Added changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,6 @@ class Product < ApplicationRecord
   has_many :product_pictures
 
   validates :name, :description, :article_number, presence: true
-  validates :article_number, numericality: { only_integer: true }
   validates :icon, :image, :pdf, :video, presence: true
 
   mount_uploader :icon, :image, PhotoUploader
