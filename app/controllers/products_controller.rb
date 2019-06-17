90e93bba280eeda1c186178e99a42b674279e659
Andreas Månsson <amanssons@gmail.com>
2019-06-17 17:03:04 +0200

    Added support for picture types and created controller and model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    
+  end
+
+  def guide
+    product = Product.find(params[:id])
+    @pictures = product.all_guide_pictures
+  end
+
+  def inspiration
+    product = Product.find(params[:id])
+    @inspirations = product.all_inspiration_pictures
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,19 @@ class Product < ApplicationRecord
   # mount_uploader :icon, :image, PhotoUploader
    # mount_uploader :url, PhotoUploader 
   # uncomment to upload images
+
+  def all_guide_pictures
+    pictures = {}
+    pictures[:guide] = product_pictures.where(picture_type: 'guide')
+    pictures[:warning] = product_pictures.where(picture_type: 'warning')
+    pictures[:tool] = product_pictures.where(picture_type: 'tool')
+    pictures[:overview] = product_pictures.where(picture_type: 'overview')
+    pictures[:screw] = product_pictures.where(picture_type: 'screw')
+    return pictures
+  end
+
+  def all_inspiration_pictures
+    return product_pictures.where(picture_type: 'inspiration')
+  end
+
 end
