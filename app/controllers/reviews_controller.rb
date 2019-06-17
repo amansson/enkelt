4dde9506371a073c3ba076e1281e0268fdc0a4f5
Rano Kuhl <ranokuhl@gmail.com>
2019-06-17 16:23:09 +0200

    Add Controller review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+  def new
+    @reviews = Review.all
+  end
+
+  def create
+    @review = Review.find(params[:review_id])
+
+    if @review.save
+      flash[:success] = "Review was created successfully"
+      redirect_to product_path(@reviews)
+    else
+      render '/'
+    end
+  end
 end
