26d9e46ba2de8b2c5195d9f17c0fa4c560cb2e05
Rano Kuhl <ranokuhl@gmail.com>
2019-06-18 15:47:45 +0200

    Review working

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,47 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @product = Product.find(params[:product_id])
   end
 
   def create
-    @review = Review.find(params[:review_id])
-
+    @product = Product.find(params[:product_id])
+    @review = Review.new(params_review)
+    @review.product = @product
+    @review.user = current_user
     if @review.save
       flash[:success] = "Review was created successfully"
       redirect_to product_path(@review.product_id)
     else
-      render '/'
+      render :new
     end
+  end
+
+  # update number in the database
+
+  def one_star
+    @track.update_attributes(rating: 1)
+  end
+
+  def two_stars
+    @track.update_attributes(rating: 2)
+  end
+
+  def three_stars
+    @track.update_attributes(rating: 3)
+  end
+
+  def four_stars
+    @track.update_attributes(rating: 4)
+  end
+
+  def five_stars
+    @track.update_attributes(rating: 5)
   end
 
   private
 
   def params_review
-    params.require(:review).permit(:rating, :content, :product_id, :user_id)
+    params.require(:review).permit(:rating, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,13 @@ Rails.application.routes.draw do
   get "products/:id/inspiration", to: "products#inspiration"
   
   get "/components", to: "pages#components"
+
+
+  post 'one_star', to: 'reviews#one_star', as: :one_star
+  post 'two_stars', to: 'reviews#two_stars', as: :two_stars
+  post 'three_stars', to: 'reviews#three_stars', as: :three_stars
+  post 'four_stars', to: 'reviews#four_stars', as: :four_stars
+  post 'five_stars', to: 'reviews#five_stars', as: :five_stars
+
+
 end
