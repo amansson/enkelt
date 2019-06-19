bd94e40d2177e9b1d70fda23a40fc01b4d7ab275
Andreas Månsson <amanssons@gmail.com>
2019-06-19 17:47:12 +0200

    think it is fixed now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,5 @@
 import "bootstrap";
 import "swiper/dist/js/swiper.min.js";
-import "swiper/dist/css/swiper.min.css";
 
 import {swipe} from "../components/swipe.js";
 swipe();
@@ -8,6 +7,6 @@ swipe();
 import {searchform} from "../components/searchform.js";
 searchform();
 
-import {billyAnimation} from "../components/billy_animation.js";
+//import {billyAnimation} from "../components/billy_animation.js";
 //billyAnimation();
 
