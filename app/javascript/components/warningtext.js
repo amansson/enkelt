6e9ec9bba01d183e96fac24caecbae01a26af357
Peter Johansson <pidd69@gmail.com>
2019-06-24 11:20:56 +0200

    Solved js error + fixed Review-controller + removed reviews from seed

--- app/javascript/components/warningtext.js
+++ app/javascript/components/warningtext.js
@@ -12,26 +12,28 @@ const warningtext = () => {
   var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+  if (btn){
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-
-   // When the user clicks on "Close" button, close the modal
-  btn_close.onclick = function() {
-    modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
       modal.style.display = "none";
     }
-  }
+
+     // When the user clicks on "Close" button, close the modal
+    btn_close.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
 };
 
 export {warningtext};
