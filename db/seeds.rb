430a11f76bef0cfd9b81dccbb0ab897dd4e190ce
Rano Kuhl <ranokuhl@gmail.com>
2019-06-25 11:14:59 +0200

    Add lamp

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,17 +22,18 @@ User.create!(id: 5, email: "fionak@gmail.com", password: "123456").save(validate
 User.create!(id: 6, email: "ranok@gmail.com", password: "123456").save(validate: false)
 
 puts 'Creating Products'
-Product.create!(id: 1, name: "Malm", description: "Chest of 2 drawers, brown stained ash veneer", article_number: "403.152.82", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560956615/ico_malm_drawer_w61gzn.png", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560956716/img_malm_drawer_ucdtoa.png", pdf: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560776545/malm_pdf_tolpxc.pdf", video: "https://www.youtube.com/watch?v=37v96zMllFA")
+Product.create!(id: 1, name: "Malm", description: "Chest of 2 drawers, brown", article_number: "403.152.82", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560956615/ico_malm_drawer_w61gzn.png", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560956716/img_malm_drawer_ucdtoa.png", pdf: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560776545/malm_pdf_tolpxc.pdf", video: "https://www.youtube.com/watch?v=37v96zMllFA")
 Product.create!(id: 2, name: "Hasselvika", description: "6-drawer chest, gray-beige", article_number: "604.217.38", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560952439/ico_hasselvika_drawer_ljazdh.jpg", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560776374/hasselvika_img_fjot9o.webp", pdf: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560776422/hasselvika_pdf_qphpve.pdf", video: "show-video")
 Product.create!(id: 3, name: "Malm", description: "Bed frame, high, black-brown, standard double", article_number: "190.069.74", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560850109/ico_malm_bed_xdramx.webp", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560850255/img_malm_bed_g2q4vh.webp", pdf: "show-pdf", video: "show-video")
 Product.create!(id: 4, name: "Brimnes", description: "Cabinet with doors, black", article_number: "803.006.60", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560850672/ico_brimnes_cabinet_lhkhu6.webp", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560850865/img_brimnes_cabinet_qu4uqi.webp", pdf: "show-pdf", video: "show-video")
 Product.create!(id: 11, name: "Malm", description: "4-drawer chest, black-brown", article_number: "603.604.62", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560853520/malm_ico_uo9vc2.jpg", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560951906/img_malm_drawer_avcr4c.png", pdf: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560776545/malm_pdf_tolpxc.pdf", video: "https://www.youtube.com/watch?v=37v96zMllFA")
-Product.create!(id: 5, name: "Brimnes", description: "Dressing table, white", article_number: "891.300.36", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1561452395/ico_brimnes_dressing_table_mycyr5.webp", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1561452447/img_brimnes_dressing_table_eucvh1.webp", pdf: "show-pdf", video: "show-video")
+Product.create!(id: 5, name: "Brimnes", description: "Dressing table, white", article_number: "702.904.59", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1561452395/ico_brimnes_dressing_table_mycyr5.webp", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1561452447/img_brimnes_dressing_table_eucvh1.webp", pdf: "show-pdf", video: "show-video")
 Product.create!(id: 6, name: "Hemnes", description: "6-drawer chest, medium brown", article_number: "403.924.78", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560950450/ico_hemnes_drawer_i1pzsz.jpg", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560950614/img_hemnes_drawer.png", pdf: "show-pdf", video: "show-video")
 Product.create!(id: 7, name: "Ingolf", description: "Chair, white", article_number: "701.032.50", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560851246/ico_ingolf_chair_xe3sqk.webp", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560851392/img_ingolf_chair_cuuafk.webp", pdf: "show-pdf", video: "show-video")
 Product.create!(id: 8, name: "Tarva", description: "Chest of 5 drawers", article_number: "502.214.19", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560954634/ico_tarva_drawer_nvbcq0.png", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560951119/img_tarva_bed_e9s6xc.png", pdf: "show-pdf", video: "show-video")
 Product.create!(id: 9, name: "Hemnes", description: "Bed frame, black-brown", article_number: "792.106.27", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560851709/ico_hemnes_bed_yx9no4.webp", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560851719/img_hemnes_bed_cjcid7.jpg", pdf: "show-pdf", video: "show-video")
 Product.create!(id: 10, name: "Tarva", description: "Bed frame, pine", article_number: "603.604.62", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1560851520/ico_tarva_bed_ranukt.webp", image: "image-url", pdf: "show-pdf", video: "show-video")
+Product.create!(id: 12, name: "Ranarp", description: "Work lamp", article_number: "903.313.88", icon: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1561453484/ico_ranarp_lamp_skjnwx.webp", image: "https://res.cloudinary.com/favapps-dx-33/image/upload/v1561453748/img_ranarp_lamp_pt7ddd.webp", pdf: "show-pdf", video: "show-video")
 
 # puts 'Creating reviews'
 # Review.create!(id: 1, user_id: 1, rating: 5, content: "Review1 some review content", product_id: 1)
@@ -87,8 +88,9 @@ ProductPicture.create!(id: 39, name: "Inspire picture", picture_type: "inspirati
 puts 'Creating User_products'
 UserProduct.create!(id: 1, user_id: 1, product_id: 1, purchase_date: "2019-07-11")
 UserProduct.create!(id: 2, user_id: 1, product_id: 2, purchase_date: "2017-07-11")
-UserProduct.create!(id: 3, user_id: 1, product_id: 3, purchase_date: "2014-07-11", assembled: true)
-UserProduct.create!(id: 4, user_id: 1, product_id: 4, purchase_date: "2012-07-11", assembled: true)
-UserProduct.create!(id: 5, user_id: 1, product_id: 5, purchase_date: "2010-07-11", assembled: true)
+UserProduct.create!(id: 3, user_id: 1, product_id: 12, purchase_date: "2017-07-11")
+UserProduct.create!(id: 4, user_id: 1, product_id: 3, purchase_date: "2014-07-11", assembled: true)
+UserProduct.create!(id: 5, user_id: 1, product_id: 4, purchase_date: "2012-07-11", assembled: true)
+UserProduct.create!(id: 6, user_id: 1, product_id: 5, purchase_date: "2010-07-11", assembled: true)
 
 
